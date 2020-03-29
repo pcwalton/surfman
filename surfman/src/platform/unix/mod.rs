ad951ef69982800d38272b98bcc231b76477a10c
Zicklag <zicklag@katharostech.com>
2020-03-28 19:40:28 -0500

    Fix Building on Linux Without X11
    Building on Linux without the `sm-x11` feature was failing. This commit
    makes sure that surfman doesn't attempt to use x11 when it is not
    compiled with the `sm-x11` feature.

--- surfman/src/platform/unix/mod.rs
+++ surfman/src/platform/unix/mod.rs
@@ -2,12 +2,18 @@
 //
 //! Backends specific to Unix-like systems, particularly Linux.
 
-#[cfg(all(unix, not(any(target_os = "macos", target_os = "android"))))]
+// The default when x11 is enabled
+#[cfg(all(feature = "sm-x11", unix, not(any(target_os = "macos", target_os = "android"))))]
 pub mod default;
+
+// The default when x11 is not enabled
+#[cfg(not(all(feature = "sm-x11", unix, not(any(target_os = "macos", target_os = "android")))))]
+pub use wayland as default;
+
 #[cfg(all(unix, not(any(target_os = "macos", target_os = "android"))))]
 pub mod generic;
+
 #[cfg(all(unix, not(any(target_os = "macos", target_os = "android"))))]
 pub mod wayland;
-#[cfg(all(any(feature = "sm-x11",
-              all(unix, not(any(target_os = "macos", target_os = "android"))))))]
+#[cfg(all(feature = "sm-x11", unix, not(any(target_os = "macos", target_os = "android"))))]
 pub mod x11;
